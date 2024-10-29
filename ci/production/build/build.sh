4fb2ae6f4cbffc90de2aba4fde65f99474ff815c
taras-turchenko-moc <taras.turchenko@masterofcode.com>
2024-10-29 22:05:51 +0200

    update build sh

--- ci/production/build/build.sh
+++ ci/production/build/build.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
+REGISTRY_HOST=localhost:32790
+
 docker build \
-  -t localhost:32780/sample-todo-app-nginx:latest \
+  -t $REGISTRY_HOST/sample-todo-app-nginx:latest \
   -f ./ci/production/build/nginx/Dockerfile \
   --build-arg API_URL=/api .
 
-docker push localhost:32780/sample-todo-app-nginx:latest
+docker push $REGISTRY_HOST/sample-todo-app-nginx:latest
 
 docker build \
-  -t localhost:32780/sample-todo-app-backend:latest \
+  -t $REGISTRY_HOST/sample-todo-app-backend:latest \
   -f ./ci/production/build/backend/Dockerfile ./apps/backend
 
-docker push localhost:32780/sample-todo-app-backend:latest
+docker push $REGISTRY_HOST/sample-todo-app-backend:latest
